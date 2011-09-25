1c8678fa2339fb4ccc6e48f5d1cac0010b003f5b
G-Qyy <github@qyy.fr>
2011-09-25 20:03:14 +0200

    Correction commentaire

--- Edf.class.php
+++ Edf.class.php
@@ -8,8 +8,8 @@ date_default_timezone_set('Europe/Paris');
 // pour rappel une constante magique CHANGE en fonction du contexte, et le
 // conseil du comm. crée une véritable constante dont la valeur serra
 // toujours la même. On risque donc de se trouver avec un __DIR__ contenant
-// une fausse valeur si ce code est appellé avant dans fichier contenu dans
-// un autre répertoire avant PHP 5.3.
+// une fausse valeur si ce code est appellé avant dans un fichier d'un autre
+// répertoire avant PHP 5.3.
 if (
   (defined(__DIR__) && __DIR__ != dirname(__FILE__))
   || !defined('__DIR__'))
